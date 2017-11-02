7671486312518eda78885c1d8cf59f12df150929
Christopher Tso <christso@gmail.com>
2017-11-02 18:07:07 +1100

    update test for batchTransfer

--- test/hdmdToken.js
+++ test/hdmdToken.js
@@ -21,39 +21,39 @@ contract('HDMDToken', function (accounts) {
             hdmd = instance;
             return hdmd.totalSupply.call();
         })
-        .then(function(_totalInitialSupply) {
-            totalInitialSupply = _totalInitialSupply.toNumber();
-        })
-        
-        // get balance of minting account before minting
-        .then(function() {
-            return hdmd.balanceOf(accounts[0]);
-        }).then(function(balance) {
-            initialBalanceOfMinter = balance.toNumber();
-        })
-        
-        // mint
-        .then(function() {
-            hdmd.allowMinter(accounts[0]);
-            hdmd.mint(mintValue);
-            return;
-        })
-        
-        // assert that the total supply increased.
-        .then(function() {
-            return hdmd.totalSupply.call();
-        }).then(function (totalSupply) {
-            totalSupply = totalSupply.toNumber();
-            assert.equal(totalSupply, totalInitialSupply + mintValue, "Minted amount did not increase total supply");
-        })
-        
-        // assert that the calling address got the new tokens.     
-        .then(function() {
-            return hdmd.balanceOf(accounts[0]);
-        }).then(function(balanceOfMinter) {
-            balanceOfMinter = balanceOfMinter.toNumber();
-            assert.equal(balanceOfMinter, initialBalanceOfMinter + mintValue, "Minted amount did not match the sending account balance");
-        })
+            .then(function (_totalInitialSupply) {
+                totalInitialSupply = _totalInitialSupply.toNumber();
+            })
+
+            // get balance of minting account before minting
+            .then(function () {
+                return hdmd.balanceOf(accounts[0]);
+            }).then(function (balance) {
+                initialBalanceOfMinter = balance.toNumber();
+            })
+
+            // mint
+            .then(function () {
+                hdmd.allowMinter(accounts[0]);
+                hdmd.mint(mintValue);
+                return;
+            })
+
+            // assert that the total supply increased.
+            .then(function () {
+                return hdmd.totalSupply.call();
+            }).then(function (totalSupply) {
+                totalSupply = totalSupply.toNumber();
+                assert.equal(totalSupply, totalInitialSupply + mintValue, "Minted amount did not increase total supply");
+            })
+
+            // assert that the calling address got the new tokens.     
+            .then(function () {
+                return hdmd.balanceOf(accounts[0]);
+            }).then(function (balanceOfMinter) {
+                balanceOfMinter = balanceOfMinter.toNumber();
+                assert.equal(balanceOfMinter, initialBalanceOfMinter + mintValue, "Minted amount did not match the sending account balance");
+            })
     });
 
     it("should not mint if minter is unauthorized", function () {
@@ -67,24 +67,24 @@ contract('HDMDToken', function (accounts) {
             hdmd = instance;
             return hdmd.totalSupply.call();
         })
-        .then(function(_totalInitialSupply) {
-            totalInitialSupply = _totalInitialSupply.toNumber();
-        })
-        
-        // mint
-        .then(function() {
-            hdmd.mint(mintValue);
-            return;
-        })
+            .then(function (_totalInitialSupply) {
+                totalInitialSupply = _totalInitialSupply.toNumber();
+            })
 
-        // get total supply
-        .then(function() {
-            return hdmd.totalSupply.call();
-        }).then(function(totalSupply) {
-            assert(totalSupply, totalInitialSupply, 
-                "Total supply cannot be increased by address that is not authorized to mint");
-        })
-        
+            // mint
+            .then(function () {
+                hdmd.mint(mintValue);
+                return;
+            })
+
+            // get total supply
+            .then(function () {
+                return hdmd.totalSupply.call();
+            }).then(function (totalSupply) {
+                assert(totalSupply, totalInitialSupply,
+                    "Total supply cannot be increased by address that is not authorized to mint");
+            })
+
     });
 
     it("should be able to transfer from wallet if you are the owner", function () {
@@ -105,16 +105,16 @@ contract('HDMDToken', function (accounts) {
         }).then(function (balance) {
             account_one_starting_balance = balance.toNumber();
             return hdmd.balanceOf.call(account_two);
-        }).then(function(balance) {
+        }).then(function (balance) {
             account_two_starting_balance = balance.toNumber();
             // transfer from account_one to account_two
-            return hdmd.transfer(account_two, amount, {from: account_one});
-        }).then(function(balance) {
+            return hdmd.transfer(account_two, amount, { from: account_one });
+        }).then(function (balance) {
             return hdmd.balanceOf.call(account_one);
-        }).then(function(balance) {
+        }).then(function (balance) {
             account_one_ending_balance = balance.toNumber();
             return hdmd.balanceOf.call(account_two);
-        }).then(function(balance) {
+        }).then(function (balance) {
             account_two_ending_balance = balance.toNumber();
             assert.equal(account_one_ending_balance, account_one_starting_balance - amount, "Amount wasn't correctly taken from the sender");
             assert.equal(account_two_ending_balance, account_two_starting_balance + amount, "Amount wasn't correctly sent to the receiver");
@@ -132,37 +132,91 @@ contract('HDMDToken', function (accounts) {
         return HDMDToken.deployed().then(function (instance) {
             hdmd = instance;
             return hdmd.totalSupply.call();
-        }).then(function(_totalInitialSupply) {
+        }).then(function (_totalInitialSupply) {
             // get supply before burn
             totalInitialSupply = _totalInitialSupply.toNumber();
-            
+
             // burn HDMD and log DMD address
             hdmd.burn(burnValue, "dbVtEGLTRYhStuL2rapCJS6yPusaWyuG5N");
-        }).then(function() {
+        }).then(function () {
             // get total supply
             return hdmd.totalSupply.call();
-        }).then(function(totalSupply) {
+        }).then(function (totalSupply) {
             totalSupply = totalSupply.toNumber();
-            assert(totalSupply, totalInitialSupply - burnValue, 
+            assert(totalSupply, totalInitialSupply - burnValue,
                 "Total supply should have decreased by the amount burned.");
         })
     });
 
-    // TODO: allocate tokens with batchTransfer
-    it("should be able to allocate a batch of tokens", function() {
+    it("should be able to allocate a batch of tokens", function () {
         var hdmd;
-        var balances = [];
-        balances.length = accounts.length;
+        var recipients = [];
+        var values = [];
+        var startingBalances = [];
+        var endingBalances = [];
+        var si = 0;
+        var ei = 0;
+        endingBalances.length = accounts.length;
+
         return HDMDToken.deployed().then(function (instance) {
             hdmd = instance;
-            // get balances for each account
-            for (var i=0; i<accounts.length; i++) {
-                hdmd.balanceOf.call(accounts[i]).then(function(balance) {
-                    balances[i] = balance.toNumber();
-                })
-            }
+
+        // get starting endingBalances for each account
+            return hdmd.balanceOf.call(accounts[si]);
+        }).then(function (balance) {
+            startingBalances[si] = balance.toNumber();
+            si = 1;
+            return hdmd.balanceOf.call(accounts[si]);
+        }).then(function (balance) {
+            startingBalances[si] = balance.toNumber();
+            si = 2;
+            return hdmd.balanceOf.call(accounts[si]);
+        }).then(function (balance) {
+            startingBalances[si] = balance.toNumber();
+            si = 3;
+            return hdmd.balanceOf.call(accounts[si]);
+        }).then(function (balance) {
+            startingBalances[si] = balance.toNumber();
+
+        // batch transfer
+            recipients = [accounts[0], accounts[1], accounts[2], accounts[3]];
+            values = [0, 1000, 900, 2000];
+            hdmd.batchTransfer(recipients, values);
         }).then(function() {
-            
+            return hdmd.balanceOf.call(accounts[ei]);
+        }).then(function(balance) {
+
+        // get ending endingBalances for each account
+            endingBalances[ei] = balance.toNumber();
+            ei = 1;
+            return hdmd.balanceOf.call(accounts[ei]);
+        }).then(function(balance) {
+            endingBalances[ei] = balance.toNumber();
+            ei = 2;
+            return hdmd.balanceOf.call(accounts[ei]);
+        }).then(function(balance) {
+            endingBalances[ei] = balance.toNumber();
+            ei = 3;
+            return hdmd.balanceOf.call(accounts[ei]);
+        }).then(function(balance) {
+            endingBalances[ei] = balance.toNumber()
+        }).then(function() {
+
+            // assert that accounts have been allocated the correct amounts
+            var a = 1;
+            assert.equal(endingBalances[a] - startingBalances[a], values[a], 
+                "Balance of account did not change by the expected value");
+            a = 2;
+            assert.equal(endingBalances[a] - startingBalances[a], values[a], 
+                "Balance of account did not change by the expected value");
+            a = 3;
+            assert.equal(endingBalances[a] - startingBalances[a], values[a], 
+                "Balance of account did not change by the expected value");
+
+            // assert that total supply is unchanged
+            var totalEndingBalance = endingBalances.reduce((a,b) => a+b, 0);
+            var totalStartingBalance = startingBalances.reduce((a,b) => a+b, 0);  
+            assert.equal(totalEndingBalance, totalStartingBalance, "Total supply should not have changed.")          
         })
     });
 });
